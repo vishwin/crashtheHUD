31d7debfcbf099ec14cf603b53c94f7f48032a8a
Charlie Li <git@vishwin.info>
2016-03-06 02:39:12 -0500

    Navigation wait function
    - Pretty print

--- navigation/__init__.py
+++ navigation/__init__.py
@@ -9,8 +9,11 @@ def launch(scr):
 
 def wait(scr):
 	keypress=None
+	wait_message=("Set driving", "directions on", "mobile phone")
 	scr.clear()
 	scr.addstr(0, int((COLS - len(title)) / 2), title)
 	scr.hline(1, 0, curses.ACS_HLINE, scr.getmaxyx()[1])
+	for i in range(2, 2 + len(wait_message)):
+		scr.addstr(i, int((COLS - len(wait_message[i - 2])) / 2), wait_message[i - 2])
 	scr.refresh()
 	keypress=scr.getch()
