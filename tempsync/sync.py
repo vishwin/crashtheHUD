65d61e3b7a3b18b2f60adb2237001da080d1b1a3
Charlie Li <git@vishwin.info>
2016-03-06 05:33:01 -0500

    Only allow left and right keys

--- tempsync/sync.py
+++ tempsync/sync.py
@@ -10,20 +10,23 @@ def confirm(scr):
 	curses.napms(2000)
 
 def sync(scr):
+	keypress=None
 	status=False
-	car_temp_str="Car temp: 24 C"
-	home_temp_str="Home temp: 14 C"
-	status_str="Status: Not synced"
-	menu_str="Cancel/Toggle"
-	if status==True:
-		status_str="Status: Synced"
-	scr.addstr(2, int((COLS - len(car_temp_str)) / 2), car_temp_str)
-	scr.addstr(3, int((COLS - len(home_temp_str)) / 2), home_temp_str)
-	scr.addstr(4, int((COLS - len(status_str)) / 2), status_str)
-	scr.addstr(5, int((COLS - len(menu_str)) / 2), menu_str)
-	scr.addch(5, 0, '<')
-	scr.addch(5, COLS - 2, '>')
-	scr.refresh()
-	if scr.getch()==261:
-		status=not status
-		return confirm(scr)
+	while keypress!=260:
+		car_temp_str="Car temp: 24 C"
+		home_temp_str="Home temp: 14 C"
+		status_str="Status: Not synced"
+		menu_str="Cancel/Toggle"
+		if status==True:
+			status_str="Status: Synced"
+		scr.addstr(2, int((COLS - len(car_temp_str)) / 2), car_temp_str)
+		scr.addstr(3, int((COLS - len(home_temp_str)) / 2), home_temp_str)
+		scr.addstr(4, int((COLS - len(status_str)) / 2), status_str)
+		scr.addstr(5, int((COLS - len(menu_str)) / 2), menu_str)
+		scr.addch(5, 0, '<')
+		scr.addch(5, COLS - 2, '>')
+		scr.refresh()
+		keypress=scr.getch()
+		if keypress==261:
+			status=not status
+			return confirm(scr)
