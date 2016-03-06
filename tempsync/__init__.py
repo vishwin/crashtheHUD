fcfd0bdd557c3e2655011d4fdd1b63e08cd7dc47
Charlie Li <git@vishwin.info>
2016-03-06 05:13:38 -0500

    Temperature sync boilerplate

--- tempsync/__init__.py
+++ tempsync/__init__.py
@@ -1,4 +1,12 @@
 from .const import *
+from .sync import *
 import curses
 
 title="Temp sync"
+
+def launch(scr):
+	scr.clear()
+	scr.addstr(0, int((COLS - len(title)) / 2), title)
+	scr.hline(1, 0, curses.ACS_HLINE, scr.getmaxyx()[1])
+	scr.refresh()
+	return sync(scr)
---
+++ tempsync/sync.py
@@ -0,0 +1,29 @@
+from .const import *
+import curses
+
+def confirm(scr):
+	msg_str="Setting updated"
+	scr.clear()
+	scr.border()
+	scr.addstr(2, int((COLS - len(msg_str)) / 2), msg_str)
+	scr.refresh()
+	curses.napms(2000)
+
+def sync(scr):
+	status=False
+	car_temp_str="Car temp: 24 C"
+	home_temp_str="Home temp: 14 C"
+	status_str="Status: Not synced"
+	menu_str="Cancel/Toggle"
+	if status==True:
+		status_str="Status: Synced"
+	scr.addstr(2, int((COLS - len(car_temp_str)) / 2), car_temp_str)
+	scr.addstr(3, int((COLS - len(home_temp_str)) / 2), home_temp_str)
+	scr.addstr(4, int((COLS - len(status_str)) / 2), status_str)
+	scr.addstr(5, int((COLS - len(menu_str)) / 2), menu_str)
+	scr.addch(5, 0, '<')
+	scr.addch(5, COLS - 2, '>')
+	scr.refresh()
+	if scr.getch()==261:
+		status=not status
+		return confirm(scr)
