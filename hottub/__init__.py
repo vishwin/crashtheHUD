f10691535af4e66e3c614cbfbfa82a2768d6fbca
Charlie Li <git@vishwin.info>
2016-03-06 03:24:50 -0500

    Complete hot tub boilerplate

--- hottub/__init__.py
+++ hottub/__init__.py
@@ -1,4 +1,12 @@
 from .const import *
+from .toggle import *
 import curses
 
 title="Hot tub"
+
+def launch(scr):
+	scr.clear()
+	scr.addstr(0, int((COLS - len(title)) / 2), title)
+	scr.hline(1, 0, curses.ACS_HLINE, scr.getmaxyx()[1])
+	scr.refresh()
+	return toggle(scr)
---
+++ hottub/toggle.py
@@ -0,0 +1,20 @@
+from .const import *
+import curses
+
+def toggle(scr):
+	keypress=None
+	status=False
+	while keypress!=260:
+		status_str="Status: Off"
+		toggle_str="Toggle"
+		if status==True:
+			status_str="Status: On"
+		scr.addstr(2, int((COLS - len(status_str)) / 2), status_str)
+		scr.addstr(3, int((COLS - len(toggle_str)) / 2), toggle_str)
+		scr.addch(3, COLS - 2, '>')
+		scr.addch(3, 0, '<')
+		scr.refresh()
+		keypress=scr.getch()
+		if keypress==261:
+			status=not status
+			scr.addstr(2, 0, " " * COLS)
